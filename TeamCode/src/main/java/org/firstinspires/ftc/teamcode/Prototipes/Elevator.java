a03be1f3cf667dcd2c7354bd2e8821934b73f162
Tevel <tevel.ftc@gmail.com>
2024-11-17 17:42:56 +0200

    elevator arm method changed to encoders, new OpMode forelevator only

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Prototipes/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Prototipes/Elevator.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Elevator {
+
     private DcMotor elevatorExtend;
     private DcMotor elevatorArm;
 
@@ -16,15 +17,32 @@ public class Elevator {
     public void initElevator(){
         elevatorExtend  = opMode.hardwareMap.get(DcMotor.class, "elevatorExtend");
         elevatorArm = opMode.hardwareMap.get(DcMotor.class, "elevatorArm");
+        elevatorArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     // this function use value (like the gamepad stick) to give the extension motor power.
     public void extend(double extension){
         elevatorExtend.setPower(extension);
+        if (elevatorExtend.getPower()<=0.3){
+            elevatorExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+    }
+    // this function use value (like the gamepad stick) to give the rotation motor power.
+    public void rotateForword(){
+//        elevatorArm.setPower(rotation);
+        if(elevatorArm.getCurrentPosition()>=20000){
+            elevatorArm.setTargetPosition(elevatorArm.getCurrentPosition());
+        }
+        else {elevatorArm.setTargetPosition(elevatorArm.getCurrentPosition()+200);} // todo check real limit numbers
+
+    }
+    public void rotateBackword(){
+//        elevatorArm.setPower(rotation);
+        if(elevatorArm.getCurrentPosition()<=20000){
+            elevatorArm.setTargetPosition(elevatorArm.getCurrentPosition());
+        }
+        else {elevatorArm.setTargetPosition(elevatorArm.getCurrentPosition()-200);} // todo check real limit numbers
     }
 
-    // this function use value (like the gamepad stick) to give the rotation motor power.
-    public void rotate(double rotation){
-        elevatorArm.setPower(rotation);
-    }
 }
+
